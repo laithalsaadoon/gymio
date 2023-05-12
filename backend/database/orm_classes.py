91e7fd97b32e347d979a1cd2309d38a897862b06
Laith Al-Saadoon <alsaadoonlaith@gmail.com>
2023-05-12 09:15:23 -0500

    docs and orm

--- backend/database/orm_classes.py
+++ backend/database/orm_classes.py
@@ -56,3 +56,60 @@ class Session(Base):
     start = Column(TIMESTAMP(timezone=True))
     end = Column(TIMESTAMP(timezone=True))
     duration = Column(Integer, Computed("extract(MINUTE from end-start)::int"))
+    nine_round_workout_id = Column(
+        Integer, ForeignKey("nine_round_workouts.id"), nullable=True
+    )
+    workout_type_id = Column(Integer, ForeignKey("workout_types.id"))
+    status_id = Column(Integer, ForeignKey("status.id"))
+
+
+class Status(Base):
+    __tablename__ = "status"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    description = Column(String)
+
+
+class SensorData(Base):
+    __tablename__ = "sensor_data"
+
+    timestamp = Column(TIMESTAMP(timezone=True), primary_key=True)
+
+    session_id = Column(Integer, ForeignKey("sessions.id"), index=True)
+    bpm = Column(Integer)
+    rr_interval_in_ms = Column(Integer)
+
+
+class NineRoundWorkout(Base):
+    __tablename__ = "nine_round_workouts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DATE)
+    # rounds 1 to 9 train and rest
+    round_1_train = Column(String)
+    round_1_rest = Column(String)
+    round_2_train = Column(String)
+    round_2_rest = Column(String)
+    round_3_train = Column(String)
+    round_3_rest = Column(String)
+    round_4_train = Column(String)
+    round_4_rest = Column(String)
+    round_5_train = Column(String)
+    round_5_rest = Column(String)
+    round_6_train = Column(String)
+    round_6_rest = Column(String)
+    round_7_train = Column(String)
+    round_7_rest = Column(String)
+    round_8_train = Column(String)
+    round_8_rest = Column(String)
+    round_9_train = Column(String)
+    round_9_rest = Column(String)
+
+
+class WorkoutType(Base):
+    __tablename__ = "workout_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    description = Column(String)
